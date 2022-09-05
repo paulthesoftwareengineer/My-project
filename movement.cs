21ec231252389c006d2e14c20e9307ed43eee891
Paul Turner <102317855+paulthesoftwareengineer@users.noreply.github.com>
2022-09-04 22:16:42 -0700

    Update movement.cs

--- movement.cs
+++ movement.cs
@@ -29,7 +29,7 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("Thrust");
+        
             rb.AddRelativeForce(Vector3.up * mainThrust *Time.deltaTime);
         }
 
